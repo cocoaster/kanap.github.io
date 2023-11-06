6643cd32250076a42e9729f07ce94fc2341ab591
cocoaster <montalbanocoralie@gmail.com>
2023-11-06 17:08:55 +0100

    minif js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,15 +1,1 @@
-const orderNumberLocation = document.getElementById("orderId");
-
-// Récupérer l'id de commande dans l'URL
-const urlParams = new URLSearchParams(window.location.search);
-const orderId = urlParams.get("orderId");
-
-// Afficher le n° de commande
-console.log(`orderId: ${orderId}`);
-orderNumberLocation.innerHTML = `${orderId}`;
-
-// Retirer les données duu local storage
-function removeLocalStorage(key) {
-  localStorage.removeItem(key);
-}
-removeLocalStorage("products");
+const orderNumberLocation=document.getElementById("orderId"),urlParams=new URLSearchParams(window.location.search),orderId=urlParams.get("orderId");function removeLocalStorage(r){localStorage.removeItem(r)}console.log(`orderId: ${orderId}`),orderNumberLocation.innerHTML=`${orderId}`,removeLocalStorage("products");
